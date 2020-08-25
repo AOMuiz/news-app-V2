907dfe100ebb0aa9752c50d7f65c798e9ff924b2
AOMuiz <54303765+AOMuiz@users.noreply.github.com>
2020-08-25 20:55:18 +0100

    First commit and added gh pages

--- src/components/Latestnews.js
+++ src/components/Latestnews.js
@@ -27,7 +27,7 @@ class LatestNews extends Component {
       console.log(response);
       this.setState({
         articles: response.articles,
-        laoding: true
+        laoding: true,
       });
     } catch (error) {
       this.setState({
@@ -44,49 +44,69 @@ class LatestNews extends Component {
         <section>
           <h2 className='text-center'>Latest Headline</h2>
           <h5 className='text-center'>
-            Continue Reading the Latest Headline Or Scroll Down to Search by Topic.
+            Continue Reading the Latest Headline Or Scroll Down to Search by
+            Topic.
             <p style={{ textAlign: "center" }}>
-            Powered by <a href='https://newsapi.org/'>NewsAPI.org</a>
-          </p>
+              Powered by <a href='https://newsapi.org/'>NewsAPI.org</a>
+            </p>
           </h5>
         </section>
-        
-        {this.state.loading && (
-            <p style={{ textAlign: "center" }}>Loading Latest articles...</p>
-          )}
 
-        { (this.state.apiError === "") ?
-        <div className='App-header cards'>
-          {this.state.articles.map((articles, index) => (
-            <div key={index + articles.title} className='container'>
-              <div className='row'>
-                <div className='col-md-4'>
-                  <div className='single-blog'>
-                    <p className='blog-meta'>
-                      By : {articles.author}
-                      <span>{articles.publishedAt.split("T")[0]} - { articles.publishedAt.split("T")[1].split('.')[0]}</span>
-                    </p>
-                    <img src={articles.urlToImage === null ? 'https://st4.depositphotos.com/14953852/22772/v/1600/depositphotos_227725020-stock-illustration-image-available-icon-flat-vector.jpg' : articles.urlToImage} alt='Not Available' />
-                    <h2>
-                      <a href={articles.url}>{articles.title}</a>
-                    </h2>
-                    <p className='blog-text'>{articles.description}</p>
-                    <p>
-                      <a href={articles.url} className='read-more-btn'>
-                        Read More
-                      </a>
-                      <span className="source">
-                      <a href={articles.url}>Source: {articles.source.name}</a>
-                    </span>
-                    </p>
+        {this.state.loading && (
+          <p style={{ textAlign: "center" }}>Loading Latest articles...</p>
+        )}
+
+        {this.state.apiError === "" ? (
+          <div className='App-header cards'>
+            {this.state.articles.map((articles, index) => (
+              <div key={index + articles.title} className='container'>
+                <div className='row'>
+                  <div className='col-md-4'>
+                    <div className='single-blog'>
+                      <p className='blog-meta'>
+                        By : {articles.author}
+                        <span>
+                          {articles.publishedAt.split("T")[0]} -{" "}
+                          {articles.publishedAt
+                            .split("T")[1]
+                            .split(".")[0]
+                            .slice(0, -1)}
+                        </span>
+                      </p>
+                      <img
+                        src={
+                          articles.urlToImage === null
+                            ? "https://st4.depositphotos.com/14953852/22772/v/1600/depositphotos_227725020-stock-illustration-image-available-icon-flat-vector.jpg"
+                            : articles.urlToImage
+                        }
+                        alt='Not Available'
+                      />
+                      <h2>
+                        <a href={articles.url}>{articles.title}</a>
+                      </h2>
+                      <p className='blog-text'>{articles.description}</p>
+                      <p className='read'>
+                        <a href={articles.url} className='read-more-btn'>
+                          Read More
+                        </a>
+                        <span className='source'>
+                          <a href={articles.url}>
+                            Source: {articles.source.name}
+                          </a>
+                        </span>
+                      </p>
+                    </div>
                   </div>
                 </div>
               </div>
-            </div>
-          ))}
-        </div>  :
-        <h3 className="error">Something went wrong: {this.state.apiError} try searching for a Topic</h3>
-      }
+            ))}
+          </div>
+        ) : (
+          <h3 className='error'>
+            Something went wrong: {this.state.apiError} try searching for a
+            Topic
+          </h3>
+        )}
       </div>
     );
   }
